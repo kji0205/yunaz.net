d2308b38d0eb65aa75421d2c86040149a7d15d6a
Jimmy <kji0205@gmail.com>
2020-12-20 10:25:22 +0900

    .

--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,24 @@
 <div>
   <h1>about</h1>
   <img src="../assets/img1.jpeg" height="665" width="1182"/>
+  <div class="right">This command will add nuxt as a dependency to your project and add it to your package.json.
+    The node_modules directory will also be created which is where all your installed packages and dependencies
+    are stored.
+    This command will add nuxt as a dependency to your project and add it to your package.json.
+    The node_modules directory will also be created which is where all your installed packages and dependencies
+    are stored.
+    This command will add nuxt as a dependency to your project and add it to your package.json.
+    The node_modules directory will also be created which is where all your installed packages and dependencies
+    are stored. <br>
+    This command will add nuxt as a dependency to your project and add it to your package.json.
+    The node_modules directory will also be created which is where all your installed packages and dependencies
+    are stored.
+    <br>
+    This command will add nuxt as a dependency to your project and add it to your package.json.
+    The node_modules directory will also be created which is where all your installed packages and dependencies
+    are stored.
+
+  </div>
 </div>
 </template>
 
